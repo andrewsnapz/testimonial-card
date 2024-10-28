ee5f57b422615c141e85e42e12ab36d6a391c61e
Andrew Shin <andyrpg@gmail.com>
2024-10-28 11:32:58 -0700

    base change

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import svgr from "vite-plugin-svgr";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), svgr()],
-  base: "/testimonial-card/",
+  base: "./",
   test: {
     globals: true,
     environment: "jsdom",
